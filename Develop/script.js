1d6291bc992038784f904d40118249f365008a5b
Victoria Malie <vmalie3@gmail.com>
2022-05-31 13:31:07 -0500

    added local storage

--- Develop/script.js
+++ Develop/script.js
@@ -2,3 +2,8 @@
 var currentDay = moment();
 $("#currentDay").text(currentDay.format("MMM Do, YYYY"));
 
+//save text to local storage
+document.querySelector('#saveBtn').addEventListener('click', function() {
+    var event = document.querySelector('#textInput').value;
+    localStorage.setItem('event', event);
+});
